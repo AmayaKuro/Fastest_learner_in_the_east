1db9ff82783dde5c589ead029a1ead25580fede8
AmayaKuro <vhtkiet9615@gmail.com>
2026-05-26 18:04:16 +0700

    done bitcask

---
+++ fat-search/bitcask/src/bitcask.rs
@@ -0,0 +1,132 @@
+use std::{
+    collections::HashMap,
+    fs::OpenOptions,
+    io::{Read, Seek, Write},
+    ops::Add,
+    path::PathBuf,
+    time::{SystemTime, UNIX_EPOCH},
+};
+
+use crc::Crc;
+
+struct LogMap {
+    file_id: String,
+    value_size: usize,
+    value_position: u64,
+    timestamp: u64,
+}
+
+pub struct Bitcask {
+    path: PathBuf,
+    log_map: HashMap<String, LogMap>,
+}
+
+impl Bitcask {
+    pub fn new(path: PathBuf) -> Self {
+        Self {
+            path: path,
+            log_map: HashMap::new(),
+        }
+    }
+
+    pub fn get(&mut self, key: String) {
+        if let Some(log_entry) = self
+            .log_map
+            .get(&self.path.to_string_lossy().to_string().add(&key))
+        {
+            match OpenOptions::new().read(true).open(&self.path) {
+                Ok(mut f) => {
+                    if let Err(e) = f.seek(std::io::SeekFrom::Start(log_entry.value_position)) {
+                        eprintln!(
+                            "Error: Failed to seek in database at {:?}: {}",
+                            self.path, e
+                        );
+                        return;
+                    }
+
+                    let mut data = vec![0; log_entry.value_size];
+                    if let Err(e) = f.read_exact(&mut data) {
+                        eprintln!(
+                            "Error: Failed to read from database at {:?}: {}",
+                            self.path, e
+                        );
+                        return;
+                    }
+                    let data = String::from_utf8_lossy(&data);
+                    let parts: Vec<&str> = data.trim_end_matches('\n').split(',').collect();
+                    if parts.len() > 5 {
+                        println!("{}", parts[5]); // crc, timestamp, key_size, value_size, key, value
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Error: Cannot access database at {:?}: {}", self.path, e);
+                    return;
+                }
+            }
+        }
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        let now = SystemTime::now();
+        let ts = now
+            .duration_since(UNIX_EPOCH)
+            .map(|d| d.as_secs())
+            .unwrap_or(0);
+        let key_size = key.len();
+        let value_size = value.len();
+
+        let data = ts.to_string()
+            + ","
+            + key_size.to_string().as_str()
+            + ","
+            + value_size.to_string().as_str()
+            + ","
+            + key.as_str()
+            + ","
+            + value.as_str();
+
+        let mut buf = Vec::new();
+        buf.extend_from_slice(data.as_bytes());
+
+        let cs = Crc::<u32>::new(&crc::CRC_32_CKSUM).checksum(&buf);
+        let buffer = format!("{},{}\n", cs, data); // data already a comma separated list
+
+        match OpenOptions::new()
+            .read(true)
+            .write(true)
+            .append(true)
+            .create(true)
+            .open(&self.path)
+        {
+            Ok(mut f) => {
+                let current_position = f.seek(std::io::SeekFrom::End(0));
+
+                let err = f.write(buffer.as_bytes());
+                if let Err(e) = err {
+                    eprintln!(
+                        "Error: Failed to write to database at {:?}: {}",
+                        self.path, e
+                    );
+                    return;
+                }
+
+                self.log_map.insert(
+                    self.path.to_string_lossy().to_string().add(&key),
+                    LogMap {
+                        file_id: self.path.to_string_lossy().to_string(),
+                        value_size: buffer.len(),
+                        value_position: current_position.unwrap_or(0),
+                        timestamp: ts,
+                    },
+                );
+                println!("Write at {:?}", self.path);
+            }
+            Err(e) => {
+                eprintln!("Error: Cannot access database at {:?}: {}", self.path, e);
+                return;
+            }
+        }
+
+        println!("SET {}={} in {:?} at {}", key, value, self.path, ts);
+    }
+}
--- fat-search/bitcask/src/main.rs
+++ fat-search/bitcask/src/main.rs
@@ -1,8 +1,8 @@
 use clap::{Parser, Subcommand};
-use crc::Crc;
-use std::fs::OpenOptions;
-use std::io::Write;
-use std::time::{SystemTime, UNIX_EPOCH};
+
+use crate::bitcask::Bitcask;
+
+mod bitcask;
 
 #[derive(Parser)]
 #[command(name = "bitcask")]
@@ -30,60 +30,16 @@ enum Action {
 
 fn main() {
     let args = Args::parse();
-
+    let mut bitcask = Bitcask::new(std::path::PathBuf::from("db/cask.1"));
+    
     match args.action {
-        Action::Get { key } => println!("GET {} from {:?}", key, args.db),
+        Action::Get { key } => {
+            bitcask.get(key);
+        }
         Action::Set { key, value } => {
-            let now = SystemTime::now();
-            let ts = now
-                .duration_since(UNIX_EPOCH)
-                .map(|d| d.as_secs())
-                .unwrap_or(0);
-            let key_size = key.len();
-            let value_size = value.len();
+            bitcask.set(key.clone(), value);
 
-            let data = ts.to_string()
-                + ","
-                + key_size.to_string().as_str()
-                + ","
-                + value_size.to_string().as_str()
-                + ","
-                + key.as_str()
-                + ","
-                + value.as_str();
-
-            let mut buf = Vec::new();
-            buf.extend_from_slice(data.as_bytes());
-
-            match OpenOptions::new()
-                .read(true)
-                .write(true)
-                .append(true)
-                .create(true)
-                .open(&args.db)
-            {
-                Ok(mut f) => {
-                    let cs = Crc::<u32>::new(&crc::CRC_32_CKSUM).checksum(&buf);
-                    let err = f.write_fmt(format_args!("{},", cs));
-                    if let Err(e) = err {
-                        eprintln!("Error: Failed to write to database at {:?}: {}", args.db, e);
-                        return;
-                    }
-                    let err = f.write_fmt(format_args!("{}", data)); // data already a comma separated list
-                    if let Err(e) = err {
-                        eprintln!("Error: Failed to write to database at {:?}: {}", args.db, e);
-                        return;
-                    }
-                    println!("Database available at {:?}", args.db);
-
-                }
-                Err(e) => {
-                    eprintln!("Error: Cannot access database at {:?}: {}", args.db, e);
-                    return;
-                }
-            }
-
-            println!("SET {}={} in {:?} at {}", key, value, args.db, ts);
+            bitcask.get(key.clone());
         }
         Action::Delete { key } => println!("DELETE {} from {:?}", key, args.db),
     }
